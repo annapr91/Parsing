2ddc0d5faa010645f4016559101d74e92b44f71e
annapr91 <89159349+annapr91@users.noreply.github.com>
2022-10-03 19:59:16 +0300

    Delete fils.py

--- parsing/fils.py
+++
@@ -1,9 +0,0 @@
-from .abst_class import BaseInterface
-
-class Books(BaseInterface):
-    def get_data(self):
-        lines = self.get_text().html.find('.T4LgNb')
-
-
-if __name__ == "__main__":
-    main()
